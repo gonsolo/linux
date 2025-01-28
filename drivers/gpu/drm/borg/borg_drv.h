7087e40fbb85931be25de5b207ec32219fc306e2
Gon Solo <gonsolo@gmail.com>
2025-01-28 16:15:02 +0100

    Read borg status.

--- drivers/gpu/drm/borg/borg_drv.h
+++ drivers/gpu/drm/borg/borg_drv.h
@@ -3,6 +3,8 @@
 #ifndef __BORG_DRV_H__
 #define __BORG_DRV_H__
 
+#include <drm/drm_file.h>
+
 #include <linux/platform_device.h>
 
 #define DRIVER_AUTHOR           "Andreas Wendleder"
@@ -10,7 +12,7 @@
 
 #define DRIVER_NAME             "borg"
 #define DRIVER_DESC             "Borg"
-#define DRIVER_DATE             "20240625"
+#define DRIVER_DATE             "20250128"
 
 #define DRIVER_MAJOR            0
 #define DRIVER_MINOR            0
@@ -18,5 +20,26 @@
 
 struct drm_device *
 borg_platform_device_create(struct platform_device*);
+
+#define BORG_PRINTK(l,c,f,a...) do {                                             \
+        struct borg_cli *_cli = (c);                                           \
+        dev_##l(_cli->drm->dev->dev, "%s: "f, _cli->name, ##a);                \
+} while(0)
+
+struct borg_drm {
+        struct drm_device *dev;
+};
+
+struct borg_cli {
+        struct borg_drm *drm;
+        char name[32];
+};
+
+static inline struct borg_cli *
+borg_cli(struct drm_file *fpriv)
+{
+        return fpriv ? fpriv->driver_priv : NULL;
+}
+
 
 #endif
